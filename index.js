aef47121ae5f50ec2fc3c9626658f9cf2bc7a8dd
mateus.badalotti <mateus.badalotti@gmail.com>
2017-04-19 13:55:44 -0300

    commit doidao
    commit doidao

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 var fs = require('fs');
 var builder = require('xmlbuilder');
+var express = require('express');
+var http = require('http');
 
-var template = builder.create('root')
-  .ele('template')
-    .att('for', 'node-js')
-    .att('to','testing-purposes')
-    .ele('repo')
-      .att('type', 'git')
-      .txt('git://github.com/oozcitak/xmlbuilder-js.git') 
-      .up()
-    .up()
-  .ele('test')
-  .txt('complete')
-.end({ pretty: true });
+// var template = builder.create('root')
+//   .ele('template')
+//     .att('for', 'node-js')
+//     .att('to','testing-purposes')
+//     .ele('repo')
+//       .att('type', 'git')
+//       .txt('git://github.com/oozcitak/xmlbuilder-js.git') 
+//       .up()
+//     .up()
+//   .ele('test')
+//   .txt('complete')
+// .end({ pretty: true });
 
-fs.writeFile("./template.xml", template, function(err) {
-    if(err) {
-        return console.log('Error while trying to write the template.xml file', err);
-    }
-    console.log("The file was saved!");
-}); 
+// fs.writeFile("./template.xml", template, function(err) {
+//     if(err) {
+//         return console.log('Error while trying to write the template.xml file', err);
+//     }
+//     console.log("The file was saved!");
+// }); 
+
+var app = express();
+
+app.use(function(req, res, next) {
+	res.append('Access-Control-Allow-Origin', '*');
+	res.append('Access-Control-Allow-Headers', 'content-type');
+	next();
+});
+
+app.post('/', function(req, res) {
+	res.json({success: true, method: 'post'});
+});
+app.get('/', function(req, res) {
+	res.json({success: true, method: 'get'});
+});
+
+var server = http.createServer(app);
+
+server.listen('8081');
---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      name: 'app'
+    }
+  },
+  created() {
+    axios.post('http://localhost:8081', {
+      firstName: 'Fred',
+      lastName: 'Flintstone'
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
